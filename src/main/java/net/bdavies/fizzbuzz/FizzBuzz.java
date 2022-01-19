337ac6d69621ece1ce92e20ebbcfe8233df4454a
Ben Davies <me@bdavies.net>
2022-01-19 14:57:28 +0000

    refactor: Fizz buzz method

--- src/main/java/net/bdavies/fizzbuzz/FizzBuzz.java
+++ src/main/java/net/bdavies/fizzbuzz/FizzBuzz.java
@@ -8,13 +8,20 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class FizzBuzz {
     public String getFizzBuzz(int number) {
-        if (number % 3 == 0 && number % 5 == 0)
-            return "FizzBuzz";
-        if (number % 5 == 0)
-            return "Buzz";
-        if (number % 3 == 0)
-            return "Fizz";
+        var str = "";
+        if (isAMultipleOfThree(number))
+            str += "Fizz";
+        if (isAMultipleOfFive(number))
+            str += "Buzz";
 
-        return String.valueOf(number);
+        return str.length() > 0 ? str : String.valueOf(number);
+    }
+
+    private boolean isAMultipleOfThree(int number) {
+        return number % 3 == 0;
+    }
+
+    private boolean isAMultipleOfFive(int number) {
+        return number % 5 == 0;
     }
 }
